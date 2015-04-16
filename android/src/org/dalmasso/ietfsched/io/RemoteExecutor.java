da3990cc9f11d5c0019683c87bf95124ee08647b
Isabelle <dalisa099@gmail.com>
2015-04-16 13:42:15 +0200

    * Changes to support IETF meetings.
    - IETF 92.

---
+++ android/src/org/dalmasso/ietfsched/io/RemoteExecutor.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dalmasso.ietfsched.io;
+
+//import com.google.android.apps.iosched.io.XmlHandler.HandlerException;
+//import com.google.android.apps.iosched.util.ParserUtils;
+
+import android.util.Log;
+
+import org.apache.http.cookie.Cookie;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+//import org.xmlpull.v1.XmlPullParser;
+//import org.xmlpull.v1.XmlPullParserException;
+
+import android.content.ContentResolver;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Executes an {@link HttpUriRequest} and passes the result as an
+ * {@link XmlPullParser} to the given {@link XmlHandler}.
+ */
+public class RemoteExecutor {
+
+	private static final String TAG = "RemoteExecutor HTTP";
+
+    private final HttpClient mHttpClient;
+
+    public RemoteExecutor(HttpClient httpClient) {
+        mHttpClient = httpClient;
+    }
+
+
+	public int executeHead(String url) throws Exception {
+		final HttpUriRequest request = new HttpHead(url);
+		final HttpResponse resp = mHttpClient.execute(request);
+		final int status = resp.getStatusLine().getStatusCode();
+        if (status != HttpStatus.SC_OK) {
+			Log.d(TAG, "Response Code " + status);
+            throw new IOException("Unexpected server response " + resp.getStatusLine() + " for " + request.getRequestLine());
+		}
+		Header h = resp.getFirstHeader("Content-Length");
+		if (h != null) {
+			try {
+				int length = Integer.parseInt(h.getValue());
+				return length;
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				return -1;
+			}	
+		}
+		return -1;
+	}
+
+    /**
+     * Execute a {@link HttpGet} request, passing a valid response through
+     * {@link XmlHandler#parseAndApply(XmlPullParser, ContentResolver)}.
+     */
+    public InputStream executeGet(String url) throws Exception {
+		// Last-Modified Thu, 01 Sep 2011 16:11:33 GMT
+		//ETag "aa4a6d-41d1-4abe37f915740"-gzip
+	
+        HttpUriRequest request = new HttpGet(url);
+		HttpResponse resp = mHttpClient.execute(request);
+        int status = resp.getStatusLine().getStatusCode();
+			
+        if (status != HttpStatus.SC_OK) {
+			Log.d(TAG, "Response Code " + status);
+			throw new IOException("Unexpected server response " + resp.getStatusLine() + " for " + request.getRequestLine());
+		}
+		return resp.getEntity().getContent();
+    }
+}
---
+++ android/src/org/dalmasso/ietfsched/ui/TracksAdapter.sv
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dalmasso.ietfsched.ui;
+
+import org.dalmasso.ietfsched.R;
+import org.dalmasso.ietfsched.provider.ScheduleContract;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+//import android.graphics.drawable.ColorDrawable;
+import android.provider.BaseColumns;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+//import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * A {@link android.widget.CursorAdapter} that renders a {@link TracksQuery}.
+ */
+public class TracksAdapter extends CursorAdapter {
+    private static final int ALL_ITEM_ID = Integer.MAX_VALUE;
+    private static final String TAG = "TrackAdapter";
+
+    private Activity mActivity;
+    private boolean mHasAllItem;
+    private int mPositionDisplacement;
+    private boolean mIsSessions = true;
+
+    public TracksAdapter(Activity activity) {
+        super(activity, null);
+        Log.d(TAG, "new track adapter");	
+        mActivity = activity;
+    }
+
+    public void setHasAllItem(boolean hasAllItem) {
+        mHasAllItem = hasAllItem;
+        mPositionDisplacement = mHasAllItem ? 1 : 0;
+    }
+
+    public void setIsSessions(boolean isSessions) {
+        mIsSessions = isSessions;
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount() + mPositionDisplacement;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+    	Log.d(TAG, "getView position" + position);
+        if (mHasAllItem && position == 0) {
+            if (convertView == null) {
+                convertView = mActivity.getLayoutInflater().inflate(
+                        R.layout.list_item_track, parent, false);
+            }
+
+            // Custom binding for the first item
+            ((TextView) convertView.findViewById(android.R.id.text1)).setText(
+                    "(" + mActivity.getResources().getString(mIsSessions
+                            ? R.string.all_sessions_title
+                            : R.string.all_sandbox_title)
+                            + ")");
+//            convertView.findViewById(android.R.id.icon1).setVisibility(View.INVISIBLE);
+
+            return convertView;
+        }
+        return super.getView(position - mPositionDisplacement, convertView, parent);
+    }
+
+    @Override
+    public Object getItem(int position) {
+         if (mHasAllItem && position == 0) {
+        	Log.d(TAG, "getItem position 0 return null");
+            return null;
+        }
+        Log.d(TAG, "getItem position " + position + " super.getItem (" + (position - mPositionDisplacement) + ") "  + super.getItem(position - mPositionDisplacement));
+        return super.getItem(position - mPositionDisplacement);
+    }
+    
+    private java.util.HashMap<Integer, Long> mId = new java.util.HashMap<Integer, Long>();
+
+    @Override
+    public long getItemId(int position) {
+        if (mHasAllItem && position == 0) {
+        	Log.d(TAG, "getItemId position = 0, return int.max");
+            return ALL_ITEM_ID;
+        }
+        long id = super.getItemId(position - mPositionDisplacement);
+        if (id != 0L) {
+        	mId.put((position - mPositionDisplacement), id);
+        	Log.d(TAG, "getItemId position / super.itemId" + position + " " + id);
+        	return id;
+        } else {
+        	Log.d(TAG, "getItemId position / saved itemId" + position + " " + mId.get(position - mPositionDisplacement));
+        	return mId.containsKey(position - mPositionDisplacement)? mId.get(position - mPositionDisplacement) : 0L;  
+        }
+//        return mid.getsuper.getItemId(position - mPositionDisplacement);
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        if (mHasAllItem && position == 0) {
+            return true;
+        }
+        return super.isEnabled(position - mPositionDisplacement);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        // Add an item type for the "All" view.
+        return super.getViewTypeCount() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mHasAllItem && position == 0) {
+            return getViewTypeCount() - 1;
+        }
+        return super.getItemViewType(position - mPositionDisplacement);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+//    	Log.d(TAG, "newView");
+        return mActivity.getLayoutInflater().inflate(R.layout.list_item_track, parent,
+                false);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        final TextView textView = (TextView) view.findViewById(android.R.id.text1);
+        textView.setText(cursor.getString(TracksQuery.TRACK_NAME));
+
+        // Assign track color to visible block
+  //      final ImageView iconView = (ImageView) view.findViewById(android.R.id.icon1);
+  //      iconView.setImageDrawable(new ColorDrawable(cursor.getInt(TracksQuery.TRACK_COLOR)));
+    }
+
+    /** {@link com.google.android.apps.iosched.provider.ScheduleContract.Tracks} query parameters. */
+    public interface TracksQuery {
+        int _TOKEN = 0x1;
+
+        String[] PROJECTION = {
+                BaseColumns._ID,
+                ScheduleContract.Tracks.TRACK_ID,
+                ScheduleContract.Tracks.TRACK_NAME,
+                ScheduleContract.Tracks.TRACK_ABSTRACT,
+                ScheduleContract.Tracks.TRACK_COLOR,
+        };
+
+        String[] PROJECTION_WITH_SESSIONS_COUNT = {
+                BaseColumns._ID,
+                ScheduleContract.Tracks.TRACK_ID,
+                ScheduleContract.Tracks.TRACK_NAME,
+                ScheduleContract.Tracks.TRACK_ABSTRACT,
+                ScheduleContract.Tracks.TRACK_COLOR,
+                ScheduleContract.Tracks.SESSIONS_COUNT,
+        };
+
+        String[] PROJECTION_WITH_VENDORS_COUNT = {
+                BaseColumns._ID,
+                ScheduleContract.Tracks.TRACK_ID,
+                ScheduleContract.Tracks.TRACK_NAME,
+                ScheduleContract.Tracks.TRACK_ABSTRACT,
+                ScheduleContract.Tracks.TRACK_COLOR,
+                ScheduleContract.Tracks.VENDORS_COUNT,
+        };
+
+        int _ID = 0;
+        int TRACK_ID = 1;
+        int TRACK_NAME = 2;
+        int TRACK_ABSTRACT = 3;
+        int TRACK_COLOR = 4;
+    }
+}
