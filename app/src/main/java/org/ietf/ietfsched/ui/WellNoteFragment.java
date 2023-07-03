b33bcccfb2053742480898a47aadeba7305aa8d5
Chris Morrow <morrowc@ops-netman.net>
2023-07-03 01:16:09 -0400

    Remove commented out text.

--- app/src/main/java/org/ietf/ietfsched/ui/WellNoteFragment.java
+++ app/src/main/java/org/ietf/ietfsched/ui/WellNoteFragment.java
@@ -40,32 +40,6 @@ public class WellNoteFragment extends Fragment {
     private static final String noteWellURL = "https://www.ietf.org/media/documents/note-well.md";
 	// private static final String default_note = R.string.note_well_default;
     public String default_note = String.valueOf(R.string.note_well_default);
-
-           /*
-            "\n\n"+
-	"Any submission to the IETF intended by the Contributor for publication as all or part of an IETF Internet-Draft "+
-	"or RFC and any statement made within the context of an IETF activity is considered an \"IETF Contribution\".\n\n Such statements "+
-	"include oral statements in IETF sessions, as well as written and electronic communications made at any time or place, which are addressed to:\n\n"+
-
-    "\t * The IETF plenary session\n"+
-    "\t * The IESG, or any member thereof on behalf of the IESG\n"+
-    "\t * Any IETF mailing list, including the IETF list itself, any working group or design team list, or any other list functioning under IETF auspices\n"+
-    "\t * Any IETF working group or portion thereof\n" +
-    "\t * Any Birds of a Feather (BOF) session\n" +
-    "\t * The IAB or any member thereof on behalf of the IAB\n" +
-    "\t * The RFC Editor or the Internet-Drafts function\n\n" +
-
-	"All IETF Contributions are subject to the rules of RFC 5378 and RFC 3979 (updated by RFC 4879).\n\n"+
-
-	"Statements made outside of an IETF session, mailing list or other function, that are clearly not intended to be input to an IETF "+ 
-	"activity, group or function, are not IETF Contributions in the context of this notice.\n\n"+
-
-	"Please consult RFC 5378 and RFC 3979 for details.\n\n"+
-
-	"A participant in any IETF activity is deemed to accept all IETF rules of process, as documented in Best Current Practices RFCs and IESG Statements.\n\n"+
-	"A participant in any IETF activity acknowledges that written, audio and video records of meetings may be made and may be available to the public.";
-            */
-
     private static final String TAG = "WellNoteFragment";
 
     @Override
