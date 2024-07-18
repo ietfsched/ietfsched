d466a98804cf73d66bc6d3f5235649759ef2ae58
Chris Morrow <morrowc@ops-netman.net>
2024-07-18 11:16:25 -0400

    [FixMonth][120] Fix the start month for ietf 120.

--- app/src/main/java/org/ietf/ietfsched/ui/ScheduleFragment.java
+++ app/src/main/java/org/ietf/ietfsched/ui/ScheduleFragment.java
@@ -92,7 +92,7 @@ public class ScheduleFragment extends Fragment implements
      * Months are the month numbers over which the meeting will span.
      * Days are the days of the month.
      */
-    private static final Integer[] months = new Integer[]{ 3 };
+    private static final Integer[] months = new Integer[]{ 7 };
     private static final Integer[] days = new Integer[]{
                    18, 19, 20, 21,
        22, 23, 24, 25, 26, 27, 28,
